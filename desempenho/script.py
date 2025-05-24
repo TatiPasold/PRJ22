e30808beab68e3883f2c337cbf655bb892dfd1f5
TatiPasold <tatypasold@gmail.com>
2025-05-24 10:06:19 -0300

    at

--- desempenho/script.py
+++ desempenho/script.py
@@ -46,7 +46,7 @@ altitude_landing = 0.0
 distance_landing = 1520.0
 MLW_frac = 0.84
 
-W0 , We , Wf , xcg_e , T0 , T0vec , S_wlan = dt.thrust_matching(
+W0, We, Wf, xcg_e, T0, T0vec, S_wlan = dt.thrust_matching(
     aircraft, W0_guess, T0_guess, TO_flap_def, LD_flap_def, TO_slat_def, LD_slat_def,
     h_ground, altitude_cruise, Mach_cruise, range_cruise, loiter_time,
     altitude_altcruise, Mach_altcruise, range_altcruise,
@@ -87,7 +87,7 @@ for S in Sw:
     new_dimensions = dt.geometry(aircraft)
     aircraft['dimensions'].update(new_dimensions)
 
-    W0 , We , Wf , xcg_e , T0 , T0vec , S_wlan = dt.thrust_matching(
+    W0, We, Wf, xcg_e, T0, T0vec, S_wlan = dt.thrust_matching(
         aircraft, W0_guess, T0_guess, TO_flap_def, LD_flap_def, TO_slat_def, LD_slat_def,
         h_ground, altitude_cruise, Mach_cruise, range_cruise, loiter_time,
         altitude_altcruise, Mach_altcruise, range_altcruise,
@@ -115,8 +115,8 @@ plt.plot(Sw, T06, label=r'$T_{0, FAR 25.121c}$', color=colors[5])
 plt.plot(Sw, T07, label=r'$T_{0, FAR 25.119}$', color=colors[6])
 plt.plot(Sw, T08, label=r'$T_{0, FAR 25.121d}$', color=colors[7])
 plt.legend()
-plt.xlabel('Wing area (m²)')
-plt.ylabel('Thrust vector (N)')
+plt.xlabel('Área de asa (m²)')
+plt.ylabel('Tração (N)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.savefig('thrust_vector_vs_wing_area.png')
 
@@ -129,13 +129,16 @@ sweeps = sweeps*np.pi/180
 
 T0s = []
 S_wlans = []
+max = []
+T_cris = [r'$T_{0, cruise}$', r'$T_{0, cruise}$', r'$T_{0, FAR 25.111}$', r'$T_{0, FAR 25.121a}$',
+          r'$T_{0, FAR 25.121b}$', r'$T_{0, FAR 25.121c}$', r'$T_{0, FAR 25.119}$', r'$T_{0, FAR 25.121d}$']
 
 for sweep in sweeps:
     aircraft['geo_param']['wing']['sweep'] = sweep
     new_dimensions = dt.geometry(aircraft)
     aircraft['dimensions'].update(new_dimensions)
 
-    W0 , We , Wf , xcg_e , T0 , T0vec , S_wlan = dt.thrust_matching(
+    W0, We, Wf, xcg_e, T0, T0vec, S_wlan = dt.thrust_matching(
         aircraft, W0_guess, T0_guess, TO_flap_def, LD_flap_def, TO_slat_def, LD_slat_def,
         h_ground, altitude_cruise, Mach_cruise, range_cruise, loiter_time,
         altitude_altcruise, Mach_altcruise, range_altcruise,
@@ -144,19 +147,27 @@ for sweep in sweeps:
         MLW_frac
     )
 
+    for i in range(len(T0vec)):
+        if T0vec[i] == T0:
+            max = T_cris[i]
+
     T0s.append(T0)
     S_wlans.append(S_wlan)
 
 plt.figure(figsize=(10, 6))
 plt.plot(sweeps*180/np.pi, T0s, label=r'$T_{0}$', color=colors[0])
-plt.xlabel('Sweep (degrees)')
-plt.ylabel('Thrust (N)')
+plt.annotate(f'Máximo: {max}', xy=(sweeps[np.argmax(T0s)]*180/np.pi, max),
+             xytext=(sweeps[np.argmax(T0s)]*180/np.pi + 2, max + 5000),
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             fontsize=10, color='black')
+plt.xlabel('Enflechamento (graus)')
+plt.ylabel('Tração (N)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.savefig('thrust_vs_aspect_ratio.png')
 
 plt.figure(figsize=(10, 6))
 plt.plot(sweeps*180/np.pi, S_wlans, label=r'$S_{wlan}$', color=colors[1])
-plt.xlabel('Sweep (degrees)')
-plt.ylabel('Wing area (m²)')
+plt.xlabel('Enflechamento (graus)')
+plt.ylabel('Área de asa (m²)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.savefig('wing_area_vs_aspect_ratio.png')
